4249fb4bbaa3dc386c913331fbaf04de7cea6cc5
Priyansh <prinshu.patel2004@gmail.com>
2026-01-07 22:30:14 +0530

    changes to host 1

--- InstaJob/server/index.js
+++ InstaJob/server/index.js
@@ -9,12 +9,20 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cors());
 app.use("/api/jobs/", require("./src/routes/JobRoute"));
 app.use("/api/auth/", require("./src/routes/AuthRoute"));
 app.use("/api/applications/", require("./src/routes/ApplicationRoute"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://your-vercel-url.vercel.app"
+    ],
+    credentials: true
+  })
+);
 
 app.listen(PORT, async () => {
   await connect_db();
