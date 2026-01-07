c8ac142ac81b59381785a8a94cd81e245aa3b31d
Priyansh <prinshu.patel2004@gmail.com>
2026-01-07 22:55:20 +0530

    solved a cors error

--- InstaJob/server/index.js
+++ InstaJob/server/index.js
@@ -9,12 +9,12 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/jobs/", require("./src/routes/JobRoute"));
 app.use("/api/auth/", require("./src/routes/AuthRoute"));
 app.use("/api/applications/", require("./src/routes/ApplicationRoute"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(morgan("dev"));
-app.use(cors());
 
 app.listen(PORT, async () => {
   await connect_db();
